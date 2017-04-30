4a7d41d0fa596aeec038a1651ad43c97f0e7ab70
ZhangCui <zczhangcui@foxmail.com>
2017-04-30 10:38:50 +0800

    update

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -11,6 +11,21 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 3000);
 
+//test
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	next();
+});
+
+app.use(function(req, res, next){
+	if (!res.locals.partials) {
+		res.locals.partials = {};
+	}
+	res.locals.partials.weather = getWeatherData();
+	next();
+});
+
+//routes
 //home
 app.get('/', function(req, res){
 	res.render('home');
@@ -26,7 +41,19 @@ var fortunes = [
 ];
 app.get('/about', function(req, res){
 	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-	res.render('about', { fortune: randomFortune });
+	res.render('about', {
+		fortune: randomFortune,
+		pageTestScript: '/qa/tests-about.js',
+	});
+});
+
+//tours/hood-river
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+//tours/request-group-rate
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
 });
 
 //404
@@ -45,3 +72,32 @@ app.use(function(err, req, res, next){
 app.listen(app.get('port'), function(){
 	console.log('Express started on http://localhost:' + app.get('port'));
 });
+
+
+function getWeatherData(){
+	return {
+		locations: [
+			{
+				name: 'Portland',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+				weather: 'Overcast',
+				temp: '54.1 F (12.3 C)',
+			},
+			{
+				name: 'Bend',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+				weather: 'Partly Cloudy',
+				temp: '55.0 F (12.8 C)',
+			},
+			{
+				name: 'Manzanita',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+				weather: 'Light Rain',
+				temp: '55.0 F (12.8 C)',
+			},
+		],
+	};
+}
